d7005aff90ebc08331a216c7b37d483449f150b4
Bruno Souto <bcovies@gmail.com>
2021-03-08 00:24:03 +0000

    Funcional!

--- builder/download-and-cut.sh
+++ builder/download-and-cut.sh
@@ -7,40 +7,30 @@ END_TIME=$2
 URL_YOUTUBE=$3
 PATH_DOWNLOAD="/home/download"
 PATH_HTTPD="/var/www/html"
+NOME_VIDEO="video.mp4"
 
 echo "=========================================================="
 echo -e "\nValores de entrada: \n\tTempo de início: $BEGIN_TIME\n\tTempo de fim: $END_TIME\n\tURL do youtube: $URL_YOUTUBE"
 echo "=========================================================="
+
+if [[ -f $PATH_HTTPD/$NOME_VIDEO ]]; then
+    ls -lth $PATH_HTTPD/$NOME_VIDEO
+    rm -rf $PATH_HTTPD/$NOME_VIDEO
+fi
+
 if curl --output /dev/null --silent --head --fail "$URL_YOUTUBE"; then
     echo -e "\nURL retornou código válido."
     echo -e "\n\tComeçando a baixar o vídeo $URL_YOUTUBE\n\n"
     echo "=========================================================="
-    youtube-dl -o "/home/download/video_baixado" $URL_YOUTUBE
+    youtube-dl -o "/home/download/$NOME_VIDEO"  --format mp4 $URL_YOUTUBE
     echo "=========================================================="
     echo -e "\nVerificando se o arquivo realmente existe:\n"
 
-    DOWNLOADED_VIDEO=$(ls /home/download | head -n 1)
-
-    if [[ -f $PATH_DOWNLOAD/$DOWNLOADED_VIDEO ]]; then
-
-        if [[ -f $PATH_HTTPD/$DOWNLOADED_VIDEO ]]; then
-            rm -rf $PATH_HTTPD/$DOWNLOADED_VIDEO
-            echo "=========================================================="
-            echo -e "\n\t\tDeletado arquivo $DOWNLOADED_VIDEO já existente em: $PATH_HTTPD"
-            echo "=========================================================="
-        else
-            echo -e "\n\t\tNenhum arquivo existente!"
-        fi
-
-        if [[ -f $PATH_DOWNLOAD/$DOWNLOADED_VIDEO ]]; then
-            echo "=========================================================="
-            echo -e "\nArquivo exitente: \n\tTempo de início: $BEGIN_TIME\n\tTempo de fim: $END_TIME\n\tURL do youtube: $URL_YOUTUBE"
-            echo "=========================================================="
-            ffmpeg -i $PATH_DOWNLOAD/$DOWNLOADED_VIDEO -ss $BEGIN_TIME -to $END_TIME -async 1 -strict -2 $PATH_HTTPD/out.mp4
-            echo "=========================================================="
-            rm -rf $PATH_DOWNLOAD/$DOWNLOADED_VIDEO
-
-        fi
+    if [[ -f $PATH_DOWNLOAD/$NOME_VIDEO ]]; then
+        echo -e "\n\nCortando o vídeo!"
+        ffmpeg -i $PATH_DOWNLOAD/$NOME_VIDEO -ss $BEGIN_TIME -to $END_TIME -async 1 -strict -2 $PATH_HTTPD/$NOME_VIDEO
+        echo "=========================================================="
+        rm -rf $PATH_DOWNLOAD/$NOME_VIDEO
     fi
 else
     echo "URL does not exist: $URL_YOUTUBE"
--- builder/php/action.php
+++ builder/php/action.php
@@ -2,8 +2,22 @@
 $inicio =  htmlspecialchars($_POST['inicio']);
 $fim = htmlspecialchars($_POST['fim']);
 $url =  htmlspecialchars($_POST['url']);
-echo $inicio;
-echo $fim;
-echo $url;
-$output = shell_exec("bash /home/scripts/download-and-cut.sh '".$inicio."'  '".$fim."'    '".$url."' ");
-echo "<pre>$output</pre>";
+$output = shell_exec("bash /home/scripts/download-and-cut.sh '" . $inicio . "'  '" . $fim . "'    '" . $url . "' ");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <a href="./video.mp4" download="video.mp4">
+    DOWNLOAD
+    </a>
+</body>
+
+</html>
